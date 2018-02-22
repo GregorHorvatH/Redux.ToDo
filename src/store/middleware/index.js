e986f74ef38b6335abf8026ee35cc16d22b4d8ba
User <User@kho-gorvat-ap.Dlink>
2018-02-22 19:41:00 +0200

    notifications

--- src/store/middleware/index.js
+++ src/store/middleware/index.js
@@ -1,6 +1,9 @@
 // Core
 import { toast, style } from 'react-toastify';
 
+// Instruments
+import types from '../../actions/todos/types';
+
 style({
     BOTTOM_RIGHT: {
         bottom:    '1em',
@@ -11,6 +14,8 @@ style({
 });
 
 export const notify = () => (next) => (action) => {
+    let message = '';
+
     if (action.error) {
         toast.error(action.payload, {
             position:  toast.POSITION.BOTTOM_RIGHT,
@@ -19,10 +24,26 @@ export const notify = () => (next) => (action) => {
     }
 
     if (action.type.indexOf('_SUCCESS') > -1) {
-        toast.success(`${action.type} - Done`, {
-            position:  toast.POSITION.BOTTOM_RIGHT,
-            autoClose: 2000,
-        });
+        switch (action.type) {
+            case types.CREATE_TODO_SUCCESS:
+                message = 'Create done!';
+                break;
+            case types.DELETE_TODO_SUCCESS:
+                message = 'Delete done!';
+                break;
+            case types.UPDATE_TODO_SUCCESS:
+                message = 'Upate done!';
+                break;
+            default:
+                break;
+        }
+
+        if (message) {
+            toast.success(message, {
+                position:  toast.POSITION.BOTTOM_RIGHT,
+                autoClose: 2000,
+            });
+        }
     }
 
     next(action);
